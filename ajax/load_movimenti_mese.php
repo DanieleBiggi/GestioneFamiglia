77afa38255909b67783b6d4ed87c5f99bdb4ea26
Daniele Biggi <74613663+DanieleBiggi@users.noreply.github.com>
2025-08-07 15:46:17 +0200

    Update load_movimenti_mese.php

--- ajax/load_movimenti_mese.php
+++ ajax/load_movimenti_mese.php
@@ -14,7 +14,7 @@ $mese = $_GET['mese'] ?? date('Y-m');
                       JOIN bilancio_etichette e ON e.id_etichetta = eo.id_etichetta
                      WHERE eo.id_tabella = bm.id_movimento_revolut AND eo.tabella_operazione='movimenti_revolut') AS etichette,
                    bm.id_gruppo_transazione, 'revolut' AS source, 'movimenti_revolut' AS tabella, null as mezzo
-            FROM movimenti_revolut bm
+            FROM v_movimenti_revolut_filtrati bm
             UNION ALL
             SELECT be.id_entrata AS id, be.descrizione_operazione AS descrizione, be.descrizione_extra,
                    be.data_operazione, be.importo AS amount,
