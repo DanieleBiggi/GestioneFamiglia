57484842613d38385ced64aeb4c706d63df1889c
Daniele Biggi <74613663+DanieleBiggi@users.noreply.github.com>
2025-08-07 10:19:33 +0200

    Ensure authenticated AJAX updates return JSON

--- ajax/salva_campo.php
+++ ajax/salva_campo.php
@@ -1,7 +1,8 @@
 <?php
+include '../includes/session_check.php';
 require_once '../includes/db.php';
 
-$id = $_POST['id'];
+$id = intval($_POST['id'] ?? 0);
 $campo = $_POST['campo'];
 $valore = $_POST['valore'];
 
@@ -11,3 +12,7 @@ if (!in_array($campo, $allowed)) exit;
 $stmt = $conn->prepare("UPDATE movimenti_revolut SET $campo = ? WHERE id_movimento_revolut = ?");
 $stmt->bind_param("si", $valore, $id);
 $stmt->execute();
+
+header('Content-Type: application/json');
+echo json_encode(['success' => true]);
+exit;
