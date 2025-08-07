ffca504ad56a113c4b697c67fd415ca556913a7d
Daniele Biggi <74613663+DanieleBiggi@users.noreply.github.com>
2025-08-07 15:24:59 +0200

    Update render_movimento.php

--- includes/render_movimento.php
+++ includes/render_movimento.php
@@ -32,7 +32,7 @@ function render_movimento(array $mov) {
         foreach (explode(',', $mov['etichette']) as $tag) {
             $tag = trim($tag);
 
-            echo '      <a href="etichetta.php?etichetta=' . urlencode($tag) . '" class="badge-etichetta me-1" onclick="event.stopPropagation();">' . htmlspecialchars($tag) . '</a>';
+            echo '      <a href="etichetta.php?id_etichetta=' . urlencode($tag) . '" class="badge-etichetta me-1" onclick="event.stopPropagation();">' . htmlspecialchars($tag) . '</a>';
 
         }
         echo '    </div>';
