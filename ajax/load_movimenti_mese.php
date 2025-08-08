0b7dbe6819491547cced27bc36737a17eb8a9c67
Daniele Biggi <74613663+DanieleBiggi@users.noreply.github.com>
2025-08-08 14:29:09 +0200

    Limit movements to current user and family

--- ajax/load_movimenti_mese.php
+++ ajax/load_movimenti_mese.php
@@ -6,8 +6,12 @@ setlocale(LC_TIME, 'it_IT.UTF-8');
 
 $mese = $_GET['mese'] ?? date('Y-m');
 
- $sql = "SELECT * FROM (
-            SELECT id_movimento_revolut AS id, COALESCE(NULLIF(descrizione_extra,''), description) AS descrizione, bm.descrizione_extra,
+$idUtente = $_SESSION['utente_id'] ?? 0;
+
+$movimenti_revolut = "";
+if (isset($_SESSION['id_famiglia_gestione']) && $_SESSION['id_famiglia_gestione'] == 1) {
+    $movimenti_revolut =
+        "SELECT id_movimento_revolut AS id, COALESCE(NULLIF(descrizione_extra,''), description) AS descrizione, bm.descrizione_extra,
                    started_date AS data_operazione, amount,
                    (SELECT GROUP_CONCAT(CONCAT(e.id_etichetta, ':', e.descrizione) SEPARATOR ',')
                       FROM bilancio_etichette2operazioni eo
@@ -15,7 +19,11 @@ $mese = $_GET['mese'] ?? date('Y-m');
                      WHERE eo.id_tabella = bm.id_movimento_revolut AND eo.tabella_operazione='movimenti_revolut') AS etichette,
                    bm.id_gruppo_transazione, 'revolut' AS source, 'movimenti_revolut' AS tabella, null as mezzo
             FROM v_movimenti_revolut_filtrati bm
-            UNION ALL
+            UNION ALL";
+}
+
+$sql = "SELECT * FROM (
+            {$movimenti_revolut}
             SELECT be.id_entrata AS id, be.descrizione_operazione AS descrizione, be.descrizione_extra,
                    be.data_operazione, be.importo AS amount,
                    (SELECT GROUP_CONCAT(CONCAT(e.id_etichetta, ':', e.descrizione) SEPARATOR ',')
@@ -24,6 +32,7 @@ $mese = $_GET['mese'] ?? date('Y-m');
                      WHERE eo.id_tabella = be.id_entrata AND eo.tabella_operazione='bilancio_entrate') AS etichette,
                    be.id_gruppo_transazione, 'ca' AS source, 'bilancio_entrate' AS tabella, be.mezzo
             FROM bilancio_entrate be
+            WHERE be.id_utente = {$idUtente}
             UNION ALL
             SELECT bu.id_uscita AS id, bu.descrizione_operazione AS descrizione, bu.descrizione_extra,
                    bu.data_operazione, -bu.importo AS amount,
@@ -33,6 +42,7 @@ $mese = $_GET['mese'] ?? date('Y-m');
                      WHERE eo.id_tabella = bu.id_uscita AND eo.tabella_operazione='bilancio_uscite') AS etichette,
                    bu.id_gruppo_transazione, 'ca' AS source, 'bilancio_uscite' AS tabella, bu.mezzo
             FROM bilancio_uscite bu
+            WHERE bu.id_utente = {$idUtente}
         ) t
         WHERE DATE_FORMAT(data_operazione, '%Y-%m') = ?
         ORDER BY data_operazione DESC";
