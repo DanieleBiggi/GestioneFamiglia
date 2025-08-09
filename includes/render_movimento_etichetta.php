16100b8412feaeb6c507a066c52c9d6f55c446d4
Daniele Biggi <74613663+DanieleBiggi@users.noreply.github.com>
2025-08-09 12:57:09 +0200

    Update render_movimento_etichetta.php

--- includes/render_movimento_etichetta.php
+++ includes/render_movimento_etichetta.php
@@ -30,87 +30,8 @@ function render_movimento_etichetta(array $mov, int $id_etichetta) {
     // Determine icon based on source
     $icon = $mov['source'] === 'revolut' ? 'assets/revolut.jpeg' : 'assets/credit.jpeg';
 
-    // Quote per utente e dati per la modal
-    $stmtU = $conn->prepare(
-        "SELECT
-	e2o.id_e2o,
-    v.descrizione,
-    u.id AS id_utente,
-    u.nome,
-    u.cognome,
-    u2o.id_u2o,
- (
-     CASE 
-     	WHEN v.id_utente_operazione = u2o.id_utente 
-     	THEN 
-     	-(
-        0
-        ) ELSE(
-        CASE WHEN IFNULL(u2o.importo_utente, 0) <> 0 THEN u2o.importo_utente ELSE(v.importo * u2o.quote)
-        END
-        )
-END
-) AS importo_utente,
-    u2o.quote,
-    u2o.saldata,
-    u2o.data_saldo,
-    v.id_utente_operazione,
-    v.importo_totale_operazione,
-    v.importo_etichetta    
-FROM
-    bilancio_utenti2operazioni_etichettate u2o
-JOIN v_bilancio_etichette2operazioni_a_testa v ON
-    u2o.id_e2o = v.id_e2o
-JOIN bilancio_etichette2operazioni e2o ON
-    e2o.id_e2o = u2o.id_e2o
-JOIN utenti u ON
-    u.id = u2o.id_utente    
-WHERE
-    u2o.id_e2o = ?
-ORDER BY
-    v.data_operazione
-DESC
-    "
-    );
-    $stmtU->bind_param('i', $mov['id_e2o']);
-    $perUser = [];
-    if ($stmtU->execute()) {
-        $resU = $stmtU->get_result();
-        $rows = [];
-        while ($r = $resU->fetch_assoc()) {
-            $rows[] = $r;
-        }
-        $count = count($rows) ?: 1;
-        foreach ($rows as $row) {
-            $quote = $row['quote'] !== null ? (float)$row['quote'] : (1 / $count);
-            $importoTot = (float)($row['importo_totale_operazione'] ?? 0);
-            $importoEtic = (float)($row['importo_etichetta'] ?? 0);
-            $importoUtente = (float)($row['importo_utente'] ?? 0);
-            $isPagante = ((int)$row['id_utente_operazione'] === (int)$row['id_utente']);
-            $imp = calcola_importo_quota($isPagante, $importoUtente, $importoEtic, $importoTot, $quote);
-            $uid = $row['id_utente'];
-            if (!isset($perUser[$uid])) {
-                $perUser[$uid] = [
-                    'nome'    => $row['nome'],
-                    'cognome' => $row['cognome'],
-                    'importo' => 0.0,
-                    'pagante' => false,
-                    'saldata' => true
-                ];
-            }
-            $perUser[$uid]['importo'] += $importoUtente;
-            /*
-            if ($isPagante) {
-                $perUser[$uid]['pagante'] = true;
-            }
-            */
-            if (!$row['saldata']) {
-                $perUser[$uid]['saldata'] = false;
-            }
-        }
-    }
-    $stmtU->close();
-
+    $perUser = get_utenti_e_quote_operazione_etichettata($mov['id_e2o']);
+    
     $rowId = 'mov-' . $mov['tabella'] . '-' . $mov['id'];
     $idE2o = $mov['id_e2o'] ?? ($info['id_e2o'] ?? 0);
     $dest = 'etichetta_dettaglio_movimento.php?id=' . (int)$idE2o;
