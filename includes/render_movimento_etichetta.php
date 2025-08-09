ff3137975e0da625e65af59d072edd2eba1b4770
Daniele Biggi <74613663+DanieleBiggi@users.noreply.github.com>
2025-08-09 12:37:51 +0200

    Update render_movimento_etichetta.php

--- includes/render_movimento_etichetta.php
+++ includes/render_movimento_etichetta.php
@@ -32,18 +32,47 @@ function render_movimento_etichetta(array $mov, int $id_etichetta) {
 
     // Quote per utente e dati per la modal
     $stmtU = $conn->prepare(
-        "SELECT e2o.id_e2o, e.descrizione AS etichetta_descrizione,
-                u.id AS id_utente, u.nome, u.cognome,
-                u2o.id_u2o, u2o.importo_utente, u2o.quote, u2o.saldata, u2o.data_saldo,
-                v.id_utente_operazione, v.importo_totale_operazione, v.importo_etichetta
-           FROM bilancio_etichette2operazioni e2o
-            JOIN bilancio_etichette e ON e.id_etichetta = e2o.id_etichetta
-            JOIN v_bilancio_etichette2operazioni_a_testa v ON v.id_e2o = e2o.id_e2o
-            JOIN bilancio_utenti2operazioni_etichettate u2o ON u2o.id_e2o = e2o.id_e2o
-            JOIN utenti u ON u.id = u2o.id_utente
-          WHERE e2o.id_tabella = ? AND e2o.tabella_operazione = ? AND e2o.id_etichetta = ?"
+        "SELECT
+	e2o.id_e2o,
+    v.descrizione,
+    u.id AS id_utente,
+    u.nome,
+    u.cognome,
+    u2o.id_u2o,
+ (
+     CASE 
+     	WHEN v.id_utente_operazione = u2o.id_utente 
+     	THEN 
+     	-(
+        0
+        ) ELSE(
+        CASE WHEN IFNULL(u2o.importo_utente, 0) <> 0 THEN u2o.importo_utente ELSE(v.importo * u2o.quote)
+        END
+        )
+END
+) AS importo_utente,
+    u2o.quote,
+    u2o.saldata,
+    u2o.data_saldo,
+    v.id_utente_operazione,
+    v.importo_totale_operazione,
+    v.importo_etichetta    
+FROM
+    bilancio_utenti2operazioni_etichettate u2o
+JOIN v_bilancio_etichette2operazioni_a_testa v ON
+    u2o.id_e2o = v.id_e2o
+JOIN bilancio_etichette2operazioni e2o ON
+    e2o.id_e2o = u2o.id_e2o
+JOIN utenti u ON
+    u.id = u2o.id_utente    
+WHERE
+    u2o.id_e2o = ?
+ORDER BY
+    v.data_operazione
+DESC
+    "
     );
-    $stmtU->bind_param('isi', $mov['id'], $mov['tabella'], $id_etichetta);
+    $stmtU->bind_param('i', $mov['id_e2o']);
     $perUser = [];
     if ($stmtU->execute()) {
         $resU = $stmtU->get_result();
@@ -69,10 +98,12 @@ function render_movimento_etichetta(array $mov, int $id_etichetta) {
                     'saldata' => true
                 ];
             }
-            $perUser[$uid]['importo'] += $imp;
+            $perUser[$uid]['importo'] += $importoUtente;
+            /*
             if ($isPagante) {
                 $perUser[$uid]['pagante'] = true;
             }
+            */
             if (!$row['saldata']) {
                 $perUser[$uid]['saldata'] = false;
             }
