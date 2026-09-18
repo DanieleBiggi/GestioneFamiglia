cda0e179db1432a2073720891cacaf05be654caf
Daniele Biggi <74613663+DanieleBiggi@users.noreply.github.com>
2026-09-18 12:11:43 +0200

    Bust cache for WebAuthn JavaScript

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 </div> <!-- Chiude container -->
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-<script src="js/webauthn.js"></script>
+<script src="/Gestionale25/js/webauthn.js?v=20260918-1"></script>
 </body>
 </html>
